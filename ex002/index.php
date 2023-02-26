ec2de1027f3260c4ea92d939a0fedc574fd7fba7
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-02-26 15:28:45 -0300

    26/02/2023

--- ex002/index.php
+++ ex002/index.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dados do Servidor</title>
+    <title>PHP</title>
 </head>
 
 <body>
-    <h1>Dados do Servidor</h1>
+    <h1>Exemplo de PHP</h1>
     <?php
-    phpinfo();
+    //date_default_timezone_get("America/Sao_Paulo");//GMT -3
+    echo "Hoje é dia " . date("d/M/Y");
+    echo " e a hora atual é " . date("G:i:s T");
     ?>
 </body>
 
